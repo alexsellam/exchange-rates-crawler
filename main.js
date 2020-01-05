da779edad2c5589e81e4e5a97a3e49b34851c409
Alex Sellam <alex.sellam@gmail.com>
2020-01-05 18:47:34 +0100

    data formatting and worker thread storage

--- main.js
+++ main.js
@@ -22,3 +22,51 @@ async function fetchData(url){
     }
     return response; 
 }
+
+let workDir = __dirname+"/dbWorker.js";
+
+const mainFunc = async () => {
+    const url = "https://www.iban.com/exchange-rates";
+    let res = await fetchData(url);
+    if(!res.data){
+        console.log("Invalid data Obj");
+        return; 
+    }
+    const html = res.data;
+
+    const $ = cheerio.load(html);
+    let dataObj = new Object();
+    const statsTable = $('.table.table-bordered.table-hover.downloads > tbody > tr');
+    statsTable.each(function(){
+        //get text in all td elements 
+        let title = $(this).find('td').text();
+        //convert string into array
+        let newStr = title.split("\t");
+        //strip off empty array element at index 0
+        newStr.shift();
+        //format array string and store in an object
+        formatStr(newStr, dataObj)
+    });
+    return dataObj;
+}
+
+mainFunc().then((res) => {
+    //start worker
+    const worker = new Worker(workDir);
+    console.log("Sending crawled data to dbWorker...");
+    //send formatted data to worker thread
+    worker.postMessage(res);
+    //listen to message from worker thread
+    worker.on("message", (message) => {
+        console.log(message);
+    });
+});
+
+function formatStr(arr, dataObj){
+    //regex to match all the works before the first digit
+    let regExp = /[^A-Z]*(^\D+)/
+    //split array element 0 using the regExp rule
+    let newArr = arr[0].split(regExp);
+    //Store Object
+    dataObj[newArr[1]] = newArr[2];
+}
